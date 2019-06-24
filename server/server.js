e39af32fbc835354eac67d8d599018adae8eee6e
Rajat Sancheti <rajat.sancheti@flipkart.com>
2019-06-24 23:45:08 +0530

    heroku

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const path = require('path')
 
 app.get('/express', (req, res) => {
   res.send({ express: 'Hello' });
 });
+
+
+app.use(express.static(path.join(__dirname, '../client/build')))
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../client/build/index.html'))
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
